767d0408fb0fe546e301452ad00f40eb3a19641f
greg <gdsuaperoii@pm.me>
2025-03-04 17:08:56 +0800

    update work experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,27 +3,39 @@ import { Section } from "./Section";
 export function Experience() {
   return (
     <Section id="experience" title="Experience">
-      <ol>
-        <li className="flex space-x-4">
-          <div className="w-1/3 text-slate-400 text-sm">
-            <div className="mb-1">April 2023 - July 2023</div>
-            <div>4 months</div>
-          </div>
-          <div className="w-2/3">
-            <h3 className="mb-1 text-slate-200 font-semibold">
-              Angular Web Developer Intern
-            </h3>
-            <span className="block mb-4 text-slate-200">
-              FastSend Mobile Solutions
-            </span>
-            <p className="text-slate-400">
-              Contributed to enhancing the company&apos;s website by adding new
-              features, addressing bugs, and optimizing existing
-              functionalities. Worked with a dedicated team of developers to
-              ensure the successful execution of these tasks.
-            </p>
-          </div>
-        </li>
+      <ol className="space-y-8">
+        {[
+          {
+            date: "Nov 2024 - Feb 2025",
+            duration: "4 months",
+            title: "Frontend Developer",
+            company: "Kunda House",
+            description:
+              "Collaborated closely with the designer, backend developers, and other stakeholders to develop and implement responsive front-end applications using React and Next.js. Identified, debugged, and resolved issues to maintain functionality and improve overall application quality.",
+          },
+          {
+            date: "April 2023 - July 2023",
+            duration: "4 months",
+            title: "Angular Web Developer Intern",
+            company: "FastSend Mobile Solutions",
+            description:
+              "Contributed to enhancing the company's website by adding new features, addressing bugs, and optimizing existing functionalities. Worked with a dedicated team of developers to ensure the successful execution of these tasks.",
+          },
+        ].map(({ date, duration, title, company, description }) => {
+          return (
+            <li key={date + company} className="flex space-x-4">
+              <div className="w-1/3 text-slate-400 text-sm">
+                <div className="mb-1">{date}</div>
+                <div>{duration}</div>
+              </div>
+              <div className="w-2/3">
+                <h3 className="mb-1 text-slate-200 font-semibold">{title}</h3>
+                <span className="block mb-4 text-slate-200">{company}</span>
+                <p className="text-slate-400">{description}</p>
+              </div>
+            </li>
+          );
+        })}
       </ol>
     </Section>
   );
